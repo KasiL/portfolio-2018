f0edd441bb3eed1e638c582c6f6f03f15cb1fa8f
Casey <kasilapp@gmail.com>
2017-12-24 13:18:15 -0800

    casey: adds script to WP enqueue

--- wp-content/themes/Divi-Kasil/src/js/custom.js
+++ wp-content/themes/Divi-Kasil/src/js/custom.js
@@ -1,14 +1,18 @@
-$(function () {
-    alert('hey Im working');
-    var h = $('#picture-container').height();
-    var picH = $('#picture-container img').height();
-    var scrollHeight = (picH - h) * -1;
-    console.log('h: ' + h);
-    console.log('picH: ' + picH);
-    console.log("scrollH: " + scrollHeight);
+(function ($) {
+    // Document Ready
+    // $(document).ready(function () {
+        alert('hey Im working');
+        var h = $('#picture-container').height();
+        var picH = $('#picture-container img').height();
+        var scrollHeight = (picH - h) * -1;
+        console.log('h: ' + h);
+        console.log('picH: ' + picH);
+        console.log("scrollH: " + scrollHeight);
 
-    $("img").draggable(
-        {
-            containment: [0, scrollHeight, 0, 0]
-        });
-});
+        $("img").draggable(
+            {
+                containment: [0, scrollHeight, 0, 0]
+            }
+        );
+    // );
+})(jQuery);
