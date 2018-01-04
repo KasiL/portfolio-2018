58588085808e23e3ffc0cab2877370202e2efdf0
Casey <kasilapp@gmail.com>
2018-01-04 00:55:26 -0800

    casey: adds SVG Animation

--- wp-content/themes/Divi-Kasil/src/js/custom/custom.js
+++ wp-content/themes/Divi-Kasil/src/js/custom/custom.js
@@ -46,5 +46,29 @@
             }
             );
         };
+        // if ($('#hero').length > 0) {
+        //     //SVG animator
+        //     var path = document.querySelector('path');
+        //     var length = path.getTotalLength();
+
+        //     // Clear any previous transition
+        //     path.style.transition = path.style.WebkitTransition =
+        //         'none';
+
+        //     // Set up the starting positions
+        //     path.style.strokeDasharray = length + ' ' + length;
+        //     path.style.strokeDashoffset = length;
+        //     // console.log( path.style.strokeDashoffset = length);
+        //     // Trigger a layout so styles are calculated & the browser 
+        //     // picks up the starting position before animating
+        //     path.getBoundingClientRect();
+
+        //     // Define our transition
+        //     path.style.transition = path.style.WebkitTransition =
+        //         'stroke-dashoffset 5s ease-in-out';
+
+        //     // Go!
+        //     path.style.strokeDashoffset = '0';
+        // }
     });
 })(jQuery);
